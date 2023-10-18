b7dfaf7e184ee9c2ce026b34503544256e2fafa5
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2023-10-18 14:13:26 +0700

    v6

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -56,10 +56,10 @@ module.exports = {
       },
       hash: true
     }),
-    // new CopyWebpackPlugin([
-    //         { from: './src/image', to: './src/image'},
-    //         { from: './src/sound', to: './src/sound'}
-    //     ])
+    new CopyWebpackPlugin([
+      { from: './assets/image', to: './build/assets/image' },
+      { from: './assets/music', to: './build/assets/music' }
+    ])
   ],
   module: {
     rules: [
